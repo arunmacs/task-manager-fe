a6be8a5a8e217e9ad9689bbfe21d9eade477fbbe
arun <aruncharymuniganti@gmail.com>
2025-11-24 19:44:52 +0530

    intro screen changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen w-full">
+      <header className="fixed top-0 left-0 bg-gray-100 w-full px-4 py-2">
+        <div className="flex justify-between items-center">
+          <div className="rounded-md text-base font-bold">TaskO</div>
+          <div className="gap-2 flex">
+            <div>
+              <button className="p-2 rounded-md text-base bg-blue-300 border border-blue-100">
+                Register
+              </button>
+            </div>
+            <div>
+              <button className="p-2 rounded-md text-base bg-emerald-300 border border-blue-100">
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="relative top-[61px] w-full">
+        <div>
+          <h1>Task Manager</h1>
+        </div>
+        <div>
+          <button onClick={() => {}}>Create Task</button>
+        </div>
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
